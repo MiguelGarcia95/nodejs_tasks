166cdea742f875f01ae36f024d610c1e0fbe5ce0
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-14 20:12:23 -0500

    validate and sanitize

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,41 +8,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true
   },
   age: {
     type: Number,
   }
 });
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  }
-})
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// })
 
-const newTask = new Task({
-  description: 'Sample Task',
-  completed: true
-})
+// const newTask = new Task({
+//   description: 'Sample Task',
+//   completed: true
+// })
 
-newTask.save()
-  .then(() => {
-    console.log(newTask);
-  }).catch((error) => {
-    console.log(error)
-  })
-
-// const me = new User({
-//   name: 'Miguel',
-//   age: 23,
-// });
-
-// me.save()
+// newTask.save()
 //   .then(() => {
-//     console.log(me);
-//   }).catch(error => {
-//     console.log('Error: ', error);
-//   });
+//     console.log(newTask);
+//   }).catch((error) => {
+//     console.log(error)
+//   })
+
+const me = new User({
+  // name: 'Miguel',
+  // age: 23,
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  }).catch(error => {
+    console.log('Error: ', error);
+  });
