1c4023be222879fe408aa091dd67646c67e01648
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-15 22:17:13 -0500

    seperate routes

--- src/index.js
+++ src/index.js
@@ -10,39 +10,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 
-
-app.post('/users', async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-      res.status(500).send(e);    
-  }
-});
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (e) {
-    res.status(500).send(e);    
-  }
-})
-
-app.get('/users/:id', async (req, res) => {
-  const {id} = req.params;
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);    
-  }
-})
-
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
   try {
