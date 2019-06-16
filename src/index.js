13001aa50759a445c027ffbfef5b813bc6c7e942
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-15 22:27:57 -0500

    seperate routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const User = require('./models/User');
 // const Task = require('./models/Task');
 const userRouter = require('./routers/user');
-const taskRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 require('./db/mongoose');
 
 const app = express();
--- src/routers/task.js
+++ src/routers/task.js
@@ -67,3 +67,5 @@ router.delete('/tasks/:id', async(req, res) => {
     res.status(500).send(e);
   }
 })
+
+module.exports = router;
