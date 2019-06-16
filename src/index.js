37cf78a360d3e27d8ebdbe58048db5386ac6b027
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-15 22:06:18 -0500

    add async to Model queries

--- src/index.js
+++ src/index.js
@@ -94,6 +94,19 @@ app.patch('/users/:id', async (req, res) => {
   }
 });
 
+app.delete('/users/:id', async(req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(400).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+})
+
 app.patch('/tasks/:id', async(req, res) => {
   const allowedUpdates = ['description', 'completed'];
   const updates = Object.keys(req.body);
@@ -104,7 +117,7 @@ app.patch('/tasks/:id', async(req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndDelete(req.params.id, req.body, {new: true, runValidators: true});
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
     
     if (!task) {
       return res.status(404).send();
@@ -113,6 +126,18 @@ app.patch('/tasks/:id', async(req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(400).send();
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send(e);
   }
 })
 
