642d5bcf48e3353d5bd1948494d8ec84965a3085
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-16 16:29:31 -0500

    pass auth  middleware to routes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,13 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
 const auth = async (req, res, next) => {
-  console.log('Auth Middleware');
-  next();
+  try {
+    const token = req.header('Authorization');
+    console.log(token)
+  } catch (e) {
+    res.status(401).send({error: 'Please Authenticate'})
+  }
 }
 
 module.exports = auth;
