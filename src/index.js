ee851c2696bab03255967cd7b068c5bdb84d842b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-15 22:07:49 -0500

    delete items

--- src/index.js
+++ src/index.js
@@ -133,7 +133,7 @@ app.delete('/tasks/:id', async(req, res) => {
   try {
     const task = await Task.findByIdAndDelete(req.params.id);
     if (!task) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
     res.send(task)
   } catch (e) {
