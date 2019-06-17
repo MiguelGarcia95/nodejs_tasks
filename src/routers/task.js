63b533548e538b5c39be12a40300ff7fa1c712ae
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-17 07:25:17 -0500

    authenticate tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -41,7 +41,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
   }
 });
 
-router.patch('/tasks/:id', async(req, res) => {
+router.patch('/tasks/:id', auth, async(req, res) => {
   const allowedUpdates = ['description', 'completed'];
   const updates = Object.keys(req.body);
   const isValidOp = updates.every(update => allowedUpdates.includes(update));
@@ -51,15 +51,14 @@ router.patch('/tasks/:id', async(req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
-    updates.forEach(update => task[update] = req.body[update]);
-    await task.save();
-
-    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    const task = await Task.findOne({_id: req.params.id, owner: req.user._id});
     
     if (!task) {
       return res.status(404).send();
     }
+
+    updates.forEach(update => task[update] = req.body[update]);
+    await task.save();
 
     res.send(task);
   } catch (e) {
