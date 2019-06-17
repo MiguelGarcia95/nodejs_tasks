a39c94b1290435f1e68e29b441250223daf448a1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-17 12:19:57 -0500

    validate upload files

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ require('./db/mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const multer = require('multer');
-// const upload = multer({
-//   dest: 'images'
-// });
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//   res.send();
-// });
+const multer = require('multer');
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a word document'));
+    }
+    cb(null, true);
+  }
+});
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send();
+});
 
 
 app.use(express.json());
