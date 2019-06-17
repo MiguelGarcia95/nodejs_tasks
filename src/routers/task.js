d87319e387ce149a2c65044a119c2740510d334f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-17 07:13:10 -0500

    authenticate tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,10 +17,10 @@ router.post('/tasks', auth, async (req, res) => {
   };
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find();
-    res.send(tasks);
+    await req.user.populate('tasks').execPopulate();
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
   }
@@ -34,7 +34,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     if (!task) {
       return res.status(404).send();
     }
-    
+
     res.send(task);
   } catch (e) {
     res.status(500).send(e);
