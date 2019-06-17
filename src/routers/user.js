0597d9796cf500776b5188cf6b609f75356dcbf6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-17 12:43:04 -0500

    validate upload files

--- src/routers/user.js
+++ src/routers/user.js
@@ -83,7 +83,18 @@ router.delete('/users/me', auth, async(req, res) => {
 
 
 
-const upload = multer({dest: 'avatars'});
+const upload = multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file,cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'));
+    }
+    res.send();
+  }
+});
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
   res.send();
