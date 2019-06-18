8caaa4a012ee61ca2302f68b603e6443c70f26c5
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-18 13:18:15 -0500

    test patch route

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,6 +1,18 @@
 const request = require('supertest');
+const app = require('../src/app');
 const Task = require('../src/models/Task');
+const {userOneId, userOne, setUpDatabase} = require('./fixtures/db');
 
-test('Should create task for user', () => {
-  
-})
+beforeEach(setUpDatabase);
+
+test('Should create task for user', async () => {
+  const response = await request(app)
+    .post('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({description: 'From my test'})
+    .expect(201)
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toEqual(false);
+}) 
