05160994f75ebb6088ccade8d587cd1e4ce95c55
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-18 12:24:57 -0500

    test create user route

--- tests/user.test.js
+++ tests/user.test.js
@@ -21,18 +21,37 @@ beforeEach(async () => {
 });
 
 test('should sign up a new user', async () => {
-  await request(app).post('/users').send({
+  const response = await request(app).post('/users').send({
     name: 'Miguel',
     email: "miguel@example.com",
     password: 'MyPass777'
   }).expect(201);
+
+  //Assert that the database was changed correctly
+  const user= await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  //Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: 'Miguel',
+      email: "miguel@example.com"
+    },
+    token: user.tokens[0].token
+  })
+
+  expect(user.password).not.toBe('MyPass777');
 });
 
 test('Should login existing user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: "mikhail@example.com",
     password: '540see!!!'
   }).expect(200);
+
+  const user = await User.findById(userOneId);
+
+  expect(response.body.user.token).toBe(user.tokens[1].token);
 })
 
 test('Should not login nonexistant user', async () => {
@@ -69,5 +88,5 @@ test('Should not delete account for unauthenticated user', async () => {
   await request(app)
     .delete('/users/me')
     .send()
-    .expect(500)
+    .expect(401)
 });
